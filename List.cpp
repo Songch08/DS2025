4dd87f99dfeb9e2acb1ccffbab38e870c39ba4ed
Songch08 <1677637984@qq.com>
2025-10-03 22:19:55 +0800

    Update List.cpp

--- List.cpp
+++ List.cpp
@@ -1,1 +1,16 @@
 #include "listNode.h"
+
+template <typename T> class List { 
+
+    private:
+    int _size;ListNodePosi(T) _header;ListNodePosi(T) _tailer;
+
+    protected:
+    void init();
+    int clear();
+    void copyNodes(ListNodePosi(T),int);
+    void merge(ListNodePosi(T)&,int,List<T>&,ListNodePosi(T),int);
+    void mergeSort(ListNodePosi(T)&,int);
+    void selectionSort(ListNodePosi(T),int);
+    void insertionSort(ListNodePosi(T),int);
+};
