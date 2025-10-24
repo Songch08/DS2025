ccde8a05afbac0f8f83b3b5ef4a8afc35e778e9c
Songch08 <1677637984@qq.com>
2025-10-25 00:04:02 +0800

    feat(vector): 添加向量头文件保护宏并实现直方图最大矩形算法  - 为 Vector.h 添加 include guard 防止重复包含

--- BinTree.h
+++ BinTree.h
@@ -35,3 +35,39 @@ template <typename T> int BinTree<T>::updateHeight(BinNodePosi(T) x)
 
 template <typename T> void BinTree<T>::updateHeightAbove(BinNodePosi(T) x)
 { while (x) {updateHeight(x); x = x->parent; } }
+
+template <typename T> BinNodePosi(T) BinTree<T>::insertAsRoot(T const& e)
+{ _size = 1;return _root = new BinNode<T>(e);}
+
+template <typename T> BinNodePosi(T) BinTree<T>::insertAsLC(BinNodePosi(T) x, T const& e)
+{ _size++;x->insertAsLC(e);updateHeightAbove(x);return x->lc;}
+
+template <typename T> BinNodePosi(T) BinTree<T>::insertAsRC(BinNodePosi(T) x, T const& e)
+{ _size++;x->insertAsRC(e);updateHeightAbove(x);return x->rc;}
+
+template <typename T>
+BinNodePosi(T) BinTree<T>::attachAsLC(BinNodePosi(T) x, BinTree<T>* &S){
+   if ( x->lc= S->_root) x->lc->parent = x;
+   _size += S->_size;updateHeightAbove(x);
+   S->_root =NULL;S->_size = 0; release (S);S=NULL;return x;
+}
+
+template <typename T>
+BinNodePosi(T) BinTree<T>::attachAsRC(BinNodePosi(T) x, BinTree<T>* &S){
+   if ( x->rc= S->_root) x->rc->parent = x;
+   _size += S->_size;updateHeightAbove(x);
+   S->_root =NULL;S->_size = 0; release (S);S=NULL;return x;
+}
+
+template <typename T> 
+int BinTree<T>::remove(BinNodePosi(T) x){ 
+   FromParentTo(*x)=NULL;
+   updateHeightAbove(x->parent);
+   int n = removeAt(x);_size -= n;return n;
+}
+template <typename T>
+static int removeAt(BinNodePosi(T) x){ 
+   if (!x) return 0;
+   int n = 1 + removeAt(x->lc) + removeAt(x->rc);
+   release(x->data);release(x);return n;
+}
